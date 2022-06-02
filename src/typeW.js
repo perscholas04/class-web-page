b76348daecd118dfbfe99b100acbdc3b9b0b8241
perscholas04 <=>
2022-06-02 14:37:24 -0400

    typed change and title

--- src/typeW.js
+++ src/typeW.js
@@ -1,19 +1,15 @@
 import TypeWriterEffect from 'react-typewriter-effect';
 import React from 'react';
-
+import Typed from 'react-typed';
+import './app.css'
 export default function TypeW(){
 
 return(
-    <TypeWriterEffect
-        textStyle={{
-          fontFamily: 'poppins',
-          color: '#2149d7',
-          fontWeight: 700,
-          fontSize: '1.5em',
-        }}
+    <Typed className={'typed'}
+        
         startDelay={2000}
         cursorColor="#3F3D56"
-        multiText={[
+        strings={[
           'What happens when the 2 best Instructors in the world get together?',
           'Class graduation rate ...... 100%',
           'AWS Cloud Practitioner Certification results... 100% Pass, 0 Failed',
@@ -23,7 +19,8 @@ return(
         ]}
         multiTextDelay={1000}
         typeSpeed={100}
-        loop={true}
+        loop
+      
       />
       
 )
